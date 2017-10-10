7286d9fb08c8e91b4454a19a95d8c8244bb10791
Luis Henrique <lholiveira@captive.apple.com>
2017-10-10 11:50:26 -0300

    Initial Commit

---
+++ nanobyte/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nanobyte
+//
+//  Created by Luis Henrique on 10/10/17.
+//  Copyright © 2017 Luis Henrique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ nanobyteTests/nanobyteTests.swift
@@ -0,0 +1,36 @@
+//
+//  nanobyteTests.swift
+//  nanobyteTests
+//
+//  Created by Luis Henrique on 10/10/17.
+//  Copyright © 2017 Luis Henrique. All rights reserved.
+//
+
+import XCTest
+@testable import nanobyte
+
+class nanobyteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
